ad310830f126a772eff540909fa069ecf2e8b475
Karolis <karolisdarbinis2@gmail.com>
2023-07-14 11:56:21 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./Functions/LocalStorage";
+
+const KEY = "persons";
 
 function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [persons, setPersons] = useState(null);
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const createPersonInfo = (_) => {
+    const person = {
+      name,
+      lastName,
+      money: 0,
+    };
+    store(KEY, person);
+    setLastName("");
+    setName("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-4">
+          <div className="card mt-4">
+            <h3 className="card-header">Add new person to bank</h3>
+            <div className="card-body">
+              <fieldset>
+                <legend>Person name</legend>
+                <div>
+                  <input type="text" value={name} onChange={handleChangeName} />
+                </div>
+                <legend>Person lastname</legend>
+                <div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={handleChangeLastName}
+                  />
+                </div>
+                <button onClick={createPersonInfo}>Add</button>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+        <div className="col-8 mt-4">Column</div>
+      </div>
     </div>
   );
 }
